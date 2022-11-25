a9fb6ca09e8a4cd84b9c3e17090f26f0cb2b6c6c
KkingKang-486 <ksh70125@naver.com>
2022-11-25 16:33:56 +0900

    preprocessing

--- job02_crawling_news_title.py
+++ job02_crawling_news_title.py
@@ -6,7 +6,7 @@ import time
 import datetime
 
 category = ['Politics', 'Economic', 'Social', 'Culture', 'World', 'IT']
-pages = [101, 101, 101, 71, 94, 73]
+pages = [101, 101, 101, 71, 94, 73] #세계 153페이지까지 있고 74까지
 url = 'https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=100#&date=%2000:00:00&page=2'
 
 options = webdriver.ChromeOptions()
@@ -14,13 +14,13 @@ options = webdriver.ChromeOptions()
 options.add_argument('lang=kr_KR')
 driver = webdriver.Chrome('./chromedriver', options=options)
 df_title = pd.DataFrame()
-for i in range(0, 6):      # section
+for i in range(4, 6):      # section  # 서현 : 섹션 5-세계,6-IT 맡았으니 0~6 => 4~6
     titles = []
-    for j in range(1, 11):   # page
+    for j in range(1, pages[i]):   # page
         url = 'https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=10{}#&date=%2000:00:00&page={}'.format(i, j)
         driver.get(url)
         time.sleep(0.2)
-        for k in range(1, 5): # x_path
+        for k in range(1, 5): # x_path  #네이버는 다 20개씩올라와서 바꿀 필요없음
             for l in range(1, 6):   # x_path
                 x_path = '//*[@id="section_body"]/ul[{}]/li[{}]/dl/dt[2]/a'.format(k, l)
                          # '//*[@id="section_body"]/ul[1]/li[4]/dl/dt[1]/a/img'
